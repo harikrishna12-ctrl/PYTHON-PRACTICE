61b8d2dc11df8c65709e2932d6d4e506b20507d0
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-13 23:04:34 +0530

    adding class and object

---
+++ python course/ooooooops/class&object.py
@@ -0,0 +1,11 @@
+class student:
+    subject="python"
+    collage="rv university"
+    year="2nd year"
+
+stud1=student()
+stud2=student()
+print(stud1.subject,stud1.collage)
+#this thing useful that we dont need to create a
+#or write every hing for every student if we just connect that 
+#then its over if we want accesesanything from the then just dot(.) and write what you need
