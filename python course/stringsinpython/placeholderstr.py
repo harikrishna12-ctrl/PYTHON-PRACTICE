3f620f3381db8e40813d7b49054766c8722b23d9
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-13 07:13:23 +0530

    added some string theory

---
+++ python course/stringsinpython/placeholderstr.py
@@ -0,0 +1,7 @@
+a=10
+b=5
+sum =a+b
+
+#normal formating
+print("the num1 {} and num2 {} sum is {}".format(a,b,sum))
+
---
+++ python course/stringsinpython/stringslice.py
@@ -0,0 +1,5 @@
+word = "i am hari krishna  reddy"
+print(word[5:])
+# we can slice a string 
+#in positive way and negative way as well
+#in negative numbering starts from last to first
