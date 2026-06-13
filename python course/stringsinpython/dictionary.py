979c2f3b9a84a56952e7506e7ad90277c5ea4071
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-13 12:06:47 +0530

    tuples and dictinary added

---
+++ python course/stringsinpython/dictionary.py
@@ -0,0 +1,28 @@
+info ={
+    "name" :"hari",
+    "cgpa"  :"8.00",
+    "subject" : ["math","science"],
+    3.14 :"PI"
+
+}
+
+print(info["cgpa"])
+
+#sictionary is unorder and mutable
+# if we want values
+dict_values=list(info.values())
+print(dict_values)
+# and same for keys
+#for ditionary items like item with key
+print(info.items())
+print(info.values())
+
+info.update({
+"name" :"chintu"
+
+})
+
+print(info["name"])
+
+
+#dictinary is mutable 
---
+++ python course/stringsinpython/tuples.py
@@ -0,0 +1,15 @@
+tuple ={2,3,4,5,3,2,3}
+print(tuple)
+#tuples are almost same as list
+#but list is mutable nut tuple is not
+# we use flower bracket
+
+
+#function in tuple
+
+#t.index-> finding a value at which index is that num
+tupl=(4,5,6,3,4,5,6,6,6)
+ind= tupl.index(6)
+print(ind)
+red= tupl.count(6)
+print(red)
