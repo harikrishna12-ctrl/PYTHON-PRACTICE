2eafd51d7dca3253b47589be9adccad1e7547deb
harikrishna <charikrishnareddy78995@gmail.com>
2026-06-14 23:42:08 +0530

    added

---
+++ python course/files&io/files&operation.py
@@ -0,0 +1,10 @@
+# f = open("python course/files&io/sample.txt", "w")
+
+# data =f.read()
+# f.write("my new things are learning")
+
+  #if we didnt mention any mood then it automatically it takes a read mode
+
+
+f = open("python course/files&io/sample2.txt", "x")
+f.write("this is my second new python file")
